e174b25d1107531a2e3cc65042f178ebcf902999
masdkm2149 <vblocks51@gmail.com>
2024-06-17 15:38:45 -0400

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/SWIFTVF/",
+  base: "https://designedbydan.art/SWIFTVF/",
   plugins: [react()],
 })
